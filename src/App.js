a4e22c03955e0937032c961b61ead6ebee393bc3
loiphamvan <loiphamvan1799@gmail.com>
2025-06-17 22:34:22 +0700

    [TICKET-01] done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React cùng mình nhé
         </a>
       </header>
     </div>
