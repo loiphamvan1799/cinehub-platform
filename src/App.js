d228662bff3189d274f3e19446aadf3d25fb1a61
doancongchin <doannine72@gmail.com>
2025-06-18 13:20:49 +0700

    [TICKET-03]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/header/Header';
-
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
