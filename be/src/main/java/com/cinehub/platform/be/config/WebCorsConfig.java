4933124f2682846e56e51955860295de53f27bf0
loiphamvan <loiphamvan1799@gmail.com>
2025-07-09 22:23:22 +0700

    config cors

--- be/src/main/java/com/cinehub/platform/be/config/WebCorsConfig.java
+++ be/src/main/java/com/cinehub/platform/be/config/WebCorsConfig.java
@@ -13,7 +13,7 @@ public class WebCorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**")
+                registry.addMapping("api/v1/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
